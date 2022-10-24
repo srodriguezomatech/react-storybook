4194c8379a0f4b1a68987f1e6ebbdc6fde2f95dc
srodriguez <srodriguez@omatech.com>
2022-10-24 15:59:20 +0200

    example

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { within, userEvent } from '@storybook/testing-library';
+import React from "react";
+import { within, userEvent } from "@storybook/testing-library";
 
-import { Page } from './Page';
+import { Dashboard } from "../components/pages/Dashboard/Dashboard";
 
 export default {
-  title: 'Example/Page',
-  component: Page,
+  title: "Pages/Dashboard",
+  component: Dashboard,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
-const Template = (args) => <Page {...args} />;
+const Template = (args) => <Dashboard {...args} />;
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const LoggedOut = Template.bind({});
@@ -20,6 +20,6 @@ export const LoggedOut = Template.bind({});
 export const LoggedIn = Template.bind({});
 LoggedIn.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+  const loginButton = await canvas.getByRole("button", { name: /Log in/i });
   await userEvent.click(loginButton);
 };
